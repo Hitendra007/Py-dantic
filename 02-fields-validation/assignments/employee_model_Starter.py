563e842e94a51e30831cea3927b93b025e27b5e8
Hitendra singh <hitendra369432@gmail.com>
2025-05-16 09:25:11 +0530

    Fields in pydantic validation optionals default values

---
+++ 02-fields-validation/assignments/employee_model_Starter.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+#todo: create Employee Model
+#fields:
+# -id: int
+# -name: str (min 3 chars)
+# -department: optional str (default "General")
+# -salary: float (must be >= 10000)
---
+++ 02-fields-validation/solutions/employee_model_Starter_solution.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class Employee(BaseModel):
+    id: int
+    name: str = Field(..., min_length=3, max_length=50,
+                      description="Employee Name", example='Hitendra singh')
+    department: Optional[str] = 'General'
+    salary: float = Field(..., ge=10000)
